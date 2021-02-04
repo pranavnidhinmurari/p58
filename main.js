182b22455e89aa4afd3c6a9cd4f1cf69ac37fc48
pranavnidhinmurari <77765414+pranavnidhinmurari@users.noreply.github.com>
2021-02-03 21:43:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+
+function callspan(){window.alert("In nature, nothing is perfect and everything is perfect. Trees can be contorted, bent in weird ways, and they’re still beautiful. –Alice Walker");}
